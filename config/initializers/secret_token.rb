205e159a242ae0d0209518caa7ffc99660982ce9
vagrant <vagrant@ironhack>
2014-03-03 10:39:15 +0000

    First Commit in Ruby on Rails curse

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timetracking::Application.config.secret_key_base = '3bf9b779af15d1830be766297aaaea420e720cba21aa653516e19b71343823c3455107ff38ae9f7c06152f0afe73f2d6d73966a670b76957f1403bf066491632'
